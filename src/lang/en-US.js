f2af46469916395bce604f8e0a8cd5c05ff5bbe1
Enix Yu <enixyu@cloudesk.top>
2018-10-16 10:52:27 +0800

    add screenshot

---
+++ src/lang/en-US.js
@@ -0,0 +1,50 @@
+export default {
+  error: {
+  },
+  constants: {
+    cancel: 'Cancel',
+    confirm: 'Confirm'
+  },
+  action: {
+    send: "Send"
+  },
+  app: {
+    langEN: 'English en-GB',
+    langZhCN: 'Simple-Chinese zh-CN'
+  },
+  welcome: {
+    servoDebugger: "Servo debugger",
+    servoSubtitle: "Single step debug for individual servo",
+    editor: "Motion Editor",
+    editorSubtitle: "Create your own set of montions"
+  },
+  debugger: {
+    selectServoFirst: "Please select a servo first",
+    targetDegree: "Target degree (n˚)",
+    duration: "Duration (ms)",
+    connectRobot: "Connect xRobot"
+  },
+  designer: {
+    add: 'Add action',
+    save: 'Save file',
+    open: 'Open file...',
+    compile: 'Compile',
+    download: 'Flash actions',
+    scanBle: 'Scan xRobot',
+    bleDisconnect: 'Disconnect',
+    selectRobot: 'Select a xRobot',
+    saveName: 'Save name',
+    rotate: '1 - Rotate',
+    sleep: '2 - Sleep',
+    goto: '3 - Goto',
+    command: 'Command',
+    value: 'Value',
+    time: 'Time',
+    edit: 'Edit'
+  },
+  blePicker: {
+    noPeripheralFound: 'No robot found',
+    turnOnBluetooth: 'Please turn on your bluetooth',
+    notSupport: 'Bluetooth not found in your machine'
+  }
+}
